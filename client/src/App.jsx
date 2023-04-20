d989abe89668f3d91cbac4b38b982fb3af834d40
Pavlo Savchuk <pavlosavchuk@iMac.Home>
2023-04-20 12:14:46 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import CanvasPane from "./canvas"
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <CanvasPane />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App

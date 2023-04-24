b56585ef891f443757d329a40b8fc1adf98ac882
Pavlo Savchuk <pavlosavchuk@iMac.Home>
2023-04-24 10:26:42 +0100

    new deployment to another server

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,8 +3,10 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://threed-shirt-kv55.onrender.com/api/v1/dalle",
+    backendUrl: "https://3d-shirt-production.up.railway.app/api/v1/dalle",
   },
 };
 
 export default config;
+
+// "https://threed-shirt-kv55.onrender.com/api/v1/dalle",

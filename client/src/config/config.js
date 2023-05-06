338b873b5c97fcce52e9e38ea3de4ceb94688caa
Pavlo Savchuk <pavlosavchuk@iMac.Home>
2023-05-06 13:47:48 +0100

    changed server api and changed glassmorphism for controller

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,8 +3,8 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    // backendUrl: "https://3d-shirt-production.up.railway.app/api/v1/dalle",
-    backendUrl: "https://threed-tshirt-server.onrender.com/api/v1/dalle",
+    backendUrl: "https://3d-t-shirt.up.railway.app/api/v1/dalle",
+    // backendUrl: "https://threed-tshirt-server.onrender.com/api/v1/dalle",
   },
 };
 
